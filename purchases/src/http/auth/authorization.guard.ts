a18d2ca7879186c98b8b392821f002e138279dc8
King kai <fabricio@me.com>
2022-06-13 23:17:20 -0400

    guard

--- purchases/src/http/auth/authorization.guard.ts
+++ purchases/src/http/auth/authorization.guard.ts
@@ -1,14 +1,46 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 import jwt from 'express-jwt';
 import { expressJwtSecret } from 'jwks-rsa';
 import { promisify } from 'node:util';
 
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
   // simplest example of injection dependency
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    return true;
+    const httpContext = context.switchToHttp();
+    const req = httpContext.getRequest();
+    const res = httpContext.getResponse();
+    const checkJWT = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: 'https://dev-vz24wox1.us.auth0.com/.well-known/jwks.json',
+        }),
+        audience: this.AUTH0_AUDIENCE,
+        issuer: this.AUTH0_DOMAIN,
+        algorithms: ['RS256'],
+      }),
+    );
+    try {
+      await checkJWT(req, res);
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
   }
 }
