4e50bae5000f91c76f363b71ad1043e5ccb6f243
German policarpo <fabriciopolicarpo0@gmail.com>
2022-07-06 21:49:09 -0400

    Rr

--- purchases/src/http/grahpql/resolvers/customer.resolver.ts
+++ purchases/src/http/grahpql/resolvers/customer.resolver.ts
@@ -1,4 +1,10 @@
-import { Resolver, Query, ResolveField, Parent } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  ResolveField,
+  Parent,
+  ResolveReference,
+} from '@nestjs/graphql';
 import { AuthUser, CurrentUser } from 'src/http/auth/current-user';
 import { CustomersService } from 'src/services/customers.service';
 import { PurchaseService } from 'src/services/purchase.service';
@@ -15,8 +21,14 @@ export class CustomerResolver {
   me(@CurrentUser() user: AuthUser) {
     return this.customersService.getCustomerByAuth(user.sub);
   }
+
   @ResolveField()
   purchases(@Parent() customer: Customer) {
     return this.purchaseService.findFromUser(customer.id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: { authUserId: string }) {
+    return this.customersService.getCustomerByAuth(reference.authUserId);
   }
 }
